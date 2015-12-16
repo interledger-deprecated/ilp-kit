c6b074d5b66d5b6eb7e471ebb0d2791eb5876d65
Malika <malika.rajvanshy@gmail.com>
2015-12-15 17:11:03 -0800

    [FIX] Form styling

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -7,28 +7,26 @@ import config from '../../config';
 import { LoginForm } from 'components';
 
 @connect(
-  state => ({
-    user: state.auth.user,
-    fail: state.auth.fail
-  }),
+  state => ({user: state.auth.user}),
   authActions)
 export default class Login extends Component {
   static propTypes = {
     user: PropTypes.object,
     login: PropTypes.func,
-    logout: PropTypes.func,
-    fail: PropTypes.object
+    logout: PropTypes.func
   }
 
   render() {
-    const {login, fail} = this.props;
+    const {login} = this.props;
     const styles = require('./Login.scss');
     return (
       <div className={styles.loginPage + ' container'}>
         <DocumentMeta title={config.app.title + ': Login'}/>
-        <h1>Login</h1>
+        <div className="col-sm-4">
+          <h1>Login</h1>
 
-        <LoginForm login={login} fail={fail} />
+          <LoginForm login={login} />
+        </div>
       </div>
     );
   }
--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -27,26 +27,28 @@ export default class Register extends Component {
     return (
       <div className={styles.registerPage + ' container'}>
         <DocumentMeta title={config.app.title + ': Register'}/>
-        <h1>Register</h1>
-          <form className="register-form" onSubmit={this.handleSubmit}>
-            <div className="row">
-              <div className="col-sm-4">
-                <div className="form-group">
-                  <label>Name</label>
-                  <input type="text" ref="name" placeholder="Enter a name" className="form-control"/>
+        <div className="col-sm-4">
+          <h1>Register</h1>
+            <form className="register-form" onSubmit={this.handleSubmit}>
+              <div className="row">
+                <div className="col-sm-12">
+                  <div className="form-group">
+                    <label>Name</label>
+                    <input type="text" ref="name" placeholder="Enter a name" className="form-control"/>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col-sm-4">
-                <div className="form-group">
-                  <label>Password</label>
-                  <input type="password" ref="password" placeholder="Enter a password" className="form-control"/>
+              <div className="row">
+                <div className="col-sm-12">
+                  <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" ref="password" placeholder="Enter a password" className="form-control"/>
+                  </div>
                 </div>
               </div>
-            </div>
-            <button type="submit" className="btn btn-success"><i className="fa fa-sign-in"/>{' '}Register</button>
-          </form>
+              <button type="submit" className="btn btn-success"><i className="fa fa-sign-in"/>{' '}Register</button>
+            </form>
+        </div>
       </div>
     );
   }
