bd03ab467b7208255bc8bde4675a9b10d1cc6c96
Vahe Hovhannisyan <vhpoet@gmail.com>
2016-01-21 17:02:37 -0800

    [TASK] Remove old test instructions

--- api/src/lib/config.js
+++ api/src/lib/config.js
@@ -9,14 +9,6 @@ module.exports = class LedgerUIConfig extends Config {
     this.parseLedgerConfig()
     this.parseDatabaseConfig()
     this.parseSessionConfig()
-
-    if (process.env.NODE_ENV === 'unit') {
-      this.server.public_host = 'localhost'
-      this.server.port = 61337
-      this.server.public_port = 80
-      this.db.uri = 'sqlite://'
-      this.updateDerivativeServerConfig()
-    }
   }
 
   parseLedgerConfig () {
