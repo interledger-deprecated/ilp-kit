430ed5136f264e2d2f2c8c1740b52d2c16aa23e8
Vahe Hovhannisyan <vhpoet@gmail.com>
2016-02-22 20:35:57 -0800

    [FIX] Ledger: Allow https interledger accounts

--- api/src/lib/utils.js
+++ api/src/lib/utils.js
@@ -3,7 +3,7 @@
 module.exports = class utils {
   static parseDestination (destination, ledgerUri) {
     // TODO Use a better mechanism to check if the destinationAccount is in a different ledger
-    if (destination.indexOf('http://') > -1) {
+    if (destination.indexOf('http://') > -1 || destination.indexOf('https://') > -1) {
       // TODO check if it's the current ledger. If yes, it's not an interledger transaction
       return {
         type: 'foreign',
