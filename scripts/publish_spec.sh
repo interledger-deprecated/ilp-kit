40991e22b5b595711255d057ad4aee8bfda3f013
Vahe Hovhannisyan <vhpoet@gmail.com>
2016-04-21 14:41:07 -0700

    [TASK] Update doc location

--- scripts/publish_spec.sh
+++ scripts/publish_spec.sh
@@ -2,8 +2,8 @@
 
 command -v apidoc || npm install -g apidoc
 git clone git@github.com:interledger/five-bells-wallet.git --branch gh-pages --single-branch doc
-apidoc -i api/src/controllers/
+apidoc -i api/src/controllers/ -o doc/
 cd doc
-git add .
+git add --all
 git commit -m '[DOCS] update spec'
 git push
