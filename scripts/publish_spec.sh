4901786da2f51a0189fad207534a637380e43886
Vahe Hovhannisyan <vhpoet@gmail.com>
2016-04-21 15:47:51 -0700

    [FIX] Docs folder -> web/docs

--- scripts/publish_spec.sh
+++ scripts/publish_spec.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 command -v apidoc || npm install -g apidoc
-git clone git@github.com:interledger/five-bells-wallet.git --branch gh-pages --single-branch doc
+git clone git@github.com:interledger/five-bells-wallet.git --branch gh-pages --single-branch web
 apidoc -i api/src/controllers/ -o web/docs
 cd web
 git add --all
