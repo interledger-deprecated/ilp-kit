796594fcbb61cc76afccae9b7276d555b418fd15
Vahe Hovhannisyan <vhpoet@gmail.com>
2016-04-25 14:57:28 -0700

    Send: support ledger URI format. Closes #14

--- api/src/lib/utils.js
+++ api/src/lib/utils.js
@@ -13,27 +13,65 @@ const NotFoundError = require('../errors/not-found-error')
 module.exports = class Utils {
   static constitute () { return [ Config, Ledger ] }
   constructor (config, ledger) {
-    this.config = config.data
     this.ledger = ledger
-    this.ledgerUri = this.config.getIn(['ledger', 'uri'])
-    this.ledgerUriPublic = this.config.getIn(['ledger', 'public_uri'])
+    this.ledgerUriPublic = config.data.getIn(['ledger', 'public_uri'])
   }
 
   isAccountUri (destination) {
     return destination.indexOf('http://') > -1 || destination.indexOf('https://') > -1
   }
 
-  getAccountType (accountUri) {
-    return this.isForeignAccountUri(accountUri) ? 'foreign' : 'local'
+  isForeignAccountUri (destination) {
+    return this.isAccountUri(destination) && destination.indexOf(this.ledgerUriPublic) === -1
   }
 
-  isForeignAccountUri (accountUri) {
-    return accountUri.indexOf(this.ledgerUriPublic) === -1
+  * getAccount (accountUri) {
+    let response
+
+    try {
+      response = yield superagent.get(accountUri).end()
+    } catch (e) {
+      throw new NotFoundError("Unknown account")
+    }
+
+    return response.body
   }
 
   isWebfinger (destination) {
     // TODO better email style checking
     return destination.search('@') > -1
+  }
+
+  * getWebfingerAccount (address) {
+    const webfinger = new WebFinger({
+      webfist_fallback: false,
+      tls_only: true,
+      uri_fallback: false,
+      request_timeout: 5000
+    });
+
+    let response
+
+    try {
+      response = yield new Promise(function(resolve, reject){
+        webfinger.lookup(address,
+          function(err, res){
+            if (err) {
+              return reject(err)
+            }
+
+            resolve(res.object)
+          }
+        )
+      })
+    } catch(e) {
+      throw new NotFoundError("Unknown account")
+    }
+
+    return {
+      accountUri: _.filter(response.links, {rel: 'http://webfinger.net/rel/ledgerAccount'})[0].href,
+      ledgerUri: _.filter(response.links, {rel: 'http://webfinger.net/rel/ledgerUri'})[0].href
+    }
   }
 
   /**
@@ -47,88 +85,46 @@ module.exports = class Utils {
     let self = this
 
     const destination = options.destination
-    const retrieveLedgerInfo = options.retrieveLedgerInfo
 
-    // Ledger account URI
-    if (self.isAccountUri(destination) && self.isForeignAccountUri(destination)) {
-      // TODO should also parse the ledger info here
-      // TODO should also retrieve ledger info here
-      // TODO should also check if exists
-      return {
-        type: self.getAccountType(destination),
-        accountUri: destination
-      }
+    let accountUri
+    let ledgerUri
+
+    // Foreign account URI
+    if (self.isForeignAccountUri(destination)) {
+      const account = yield self.getAccount(destination)
+
+      accountUri = destination
+      ledgerUri = account.ledger
     }
 
     // Webfinger
     else if (self.isWebfinger(destination)) {
-      var webfinger = new WebFinger({
-        webfist_fallback: false,
-        tls_only: true,
-        uri_fallback: false,
-        request_timeout: 5000
-      });
+      const account = yield self.getWebfingerAccount(destination)
 
-      let data
-
-      try {
-        data = yield new Promise(function(resolve, reject){
-          webfinger.lookup(destination,
-            function(err, res){
-              if (err) {
-                return reject(err)
-              }
-
-              resolve(res.object)
-            }
-          )
-        })
-      } catch(e) {
-        throw new NotFoundError("Unknown account")
-      }
-
-      let accountUri = _.filter(data.links, {rel: 'http://webfinger.net/rel/ledgerAccount'})[0].href
-      let ledgerUri = _.filter(data.links, {rel: 'http://webfinger.net/rel/ledgerUri'})[0].href
-
-      let parsedDestination = {
-        type: self.getAccountType(accountUri),
-        accountUri: accountUri,
-        ledgerUri: ledgerUri
-      }
-
-      if (retrieveLedgerInfo) {
-        parsedDestination.ledgerInfo = yield self.ledger.getInfo(parsedDestination.ledgerUri)
-      }
-
-      return parsedDestination
+      accountUri = account.accountUri
+      ledgerUri = account.ledgerUri
     }
 
     // Local account
     else {
-      let accountUri = self.isAccountUri(destination)
-        ? destination
-        : self.ledgerUriPublic + '/accounts/' + destination
-
-      // Local account
-      let parsedDestination = {
-        type: self.getAccountType(accountUri),
-        accountUri: accountUri,
-        ledgerUri: self.ledgerUriPublic
-      }
+      accountUri = self.isAccountUri(destination) ? destination : self.ledgerUriPublic + '/accounts/' + destination
+      ledgerUri = self.ledgerUriPublic
 
       // Check if account exists
-      try {
-        yield superagent.get(accountUri).end()
-      } catch (e) {
-        throw new NotFoundError("Unknown account")
-      }
-      
-      // TODO:PERFORMANCE api should already know the current ledgerInfo at this point
-      if (retrieveLedgerInfo) {
-        parsedDestination.ledgerInfo = yield self.ledger.getInfo(parsedDestination.ledgerUri)
-      }
-
-      return parsedDestination
+      yield self.getAccount(accountUri)
     }
+
+    let parsedDestination = {
+      type: this.isForeignAccountUri(accountUri) ? 'foreign' : 'local',
+      accountUri: accountUri,
+      ledgerUri: ledgerUri
+    }
+
+    // TODO:PERFORMANCE api should already know the current ledgerInfo at this point
+    if (options.retrieveLedgerInfo) {
+      parsedDestination.ledgerInfo = yield self.ledger.getInfo(parsedDestination.ledgerUri)
+    }
+
+    return parsedDestination
   }
 }
