7584d5043d3988f59eaace8035ae09d12e024d05
CircleCI <info@circleci.com>
2016-04-27 18:13:49 +0000

    [DOCS] update spec

--- docs/api_project.js
+++ docs/api_project.js
@@ -14,7 +14,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-04-26T23:44:46.794Z",
+    "time": "2016-04-27T18:13:48.892Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
