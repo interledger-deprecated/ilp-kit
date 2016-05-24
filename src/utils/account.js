dbcf527f4afed714a6d6110e59937ad78577f70c
Vahe Hovhannisyan <vhpoet@gmail.com>
2016-05-24 15:07:56 -0700

    utils: fix the getAccountName regex. closes #17

--- src/utils/account.js
+++ src/utils/account.js
@@ -1,7 +1,7 @@
 // account formatting
 export function getAccountName(account) {
   if (account.indexOf('http://') > -1 || account.indexOf('https://') > -1) {
-    const match = account.match(/.*\/([a-z0-9]*)/)
+    const match = account.match(/.*\/([-a-z0-9]*)/)
     return match[1]
   }
 
