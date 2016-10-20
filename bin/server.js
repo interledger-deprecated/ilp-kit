a7fa6915ec1430c6ebc4ea9122965bfd2c124281
Vahe Hovhannisyan <vhpoet@gmail.com>
2016-10-20 11:48:21 -0700

    feat: ignore hot reloadable files from piping

--- bin/server.js
+++ bin/server.js
@@ -13,7 +13,7 @@ global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
 if (__DEVELOPMENT__) {
   if (!require('piping')({
       hook: true,
-      ignore: /(\/\.|~$|\.json|\.scss$)/i
+      ignore: /(\/\.|~$|\.json|\.scss|components|containers|decorators|redux$)/i
     })) {
     return;
   }
