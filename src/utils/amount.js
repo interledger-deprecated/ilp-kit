95850bdaa515ee0baa85ede4a1d0001e22b207e8
Vahe Hovhannisyan <vhpoet@gmail.com>
2016-11-09 13:55:59 -0800

    fix: amount formatter work with negative values fixes #48

--- src/utils/amount.js
+++ src/utils/amount.js
@@ -1,7 +1,8 @@
 // Amount formatting
 export function amount(num) {
   let am = parseFloat(num)
-  am = am > 1 ? am.toFixed(2) : am.toPrecision(2)
+
+  am = (am > 1 || am < -1) ? am.toFixed(2) : am.toPrecision(2)
 
   return am.replace(/\.?0+$/, '')
 }
