11783f58b80868103c6674ce560632324ad696a2
Vahe Hovhannisyan <vhpoet@gmail.com>
2016-11-23 16:39:05 -0800

    chore: wording on 404 page

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function NotFound() {
   return (
-    <div className="container">
-      <h1>Doh! 404!</h1>
-      <p>These are <em>not</em> the droids you are looking for!</p>
+    <div>
+      <h1>Not Found!</h1>
+      <p>The requested page hasn't been found</p>
     </div>
-  );
+  )
 }
