0a7b75857de25ed2aa909fe0260016e91fd75e22
Ben Sharafian <benms64@gmail.com>
2016-12-21 15:49:35 +0100

    fix: make sql queries compatible with sqlite

--- api/src/models/payment.js
+++ api/src/models/payment.js
@@ -78,7 +78,7 @@ function PaymentFactory (sequelize, validator, container, User) {
           + ' destination_name,'
           + ' destination_image_url,'
           + ' message,'
-          + ' date_trunc(\'hour\', created_at) AS time_slot,'
+          + ' created_at AS time_slot,'
           + ' max(created_at) AS recent_date,'
           + ' count(*) as transfers_count'
         + ' FROM "Payments"'
@@ -93,8 +93,8 @@ function PaymentFactory (sequelize, validator, container, User) {
           + 'destination_identifier, destination_name, destination_image_url,'
           + ' message, time_slot'
         + ' ORDER BY recent_date DESC'
-        + ' OFFSET ' + limit * (page - 1)
-        + ' LIMIT ' + limit,
+        + ' LIMIT ' + limit
+        + ' OFFSET ' + limit * (page - 1),
         {model: Payment.DbModel}
       )
 
@@ -104,7 +104,7 @@ function PaymentFactory (sequelize, validator, container, User) {
           + ' sum(source_amount) as source_amount,'
           + ' sum(destination_amount) as destination_amount,'
           + ' message,'
-          + ' date_trunc(\'hour\', created_at) AS time_slot'
+          + ' created_at AS time_slot'
         + ' FROM "Payments"'
         + ' WHERE state = \'success\' '
           + ' AND ('
@@ -130,7 +130,7 @@ function PaymentFactory (sequelize, validator, container, User) {
       + ' WHERE state = \'success\' '
         + " AND source_identifier = '" + params.sourceIdentifier + "'"
         + " AND destination_identifier = '" + params.destinationIdentifier + "'"
-        + " AND date_trunc('hour', created_at) = '" + params.timeSlot + "'"
+        + " AND created_at = '" + params.timeSlot + "'"
         + (params.message ? " AND message = '" + params.message + "'" : '')
       + ' ORDER BY created_at DESC',
         {model: Payment.DbModel}
@@ -201,7 +201,7 @@ function PaymentFactory (sequelize, validator, container, User) {
       type: Sequelize.STRING(512),
       unique: true
     },
-    state: Sequelize.ENUM('pending', 'success', 'fail'),
+    state: Sequelize.STRING(10),
     message: Sequelize.STRING(1024), // TODO decide on the size
     execution_condition: Sequelize.STRING(1024), // TODO decide on the size
     created_at: Sequelize.DATE,
@@ -210,7 +210,6 @@ function PaymentFactory (sequelize, validator, container, User) {
     indexes: [
       {
         name: 'Payments_execution_condition_idx',
-        method: 'BTREE',
         fields: ['execution_condition']
       }
     ]
