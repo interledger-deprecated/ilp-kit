e61298849f47606f667080042d5bee00589ed54e
Vahe Hovhannisyan <vhpoet@gmail.com>
2016-12-22 17:25:26 -0800

    feat: connector: add plugin skeleton

--- api/src/lib/connector.js
+++ api/src/lib/connector.js
@@ -1,28 +1,45 @@
 "use strict"
 
+const co = require('co')
 const request = require('superagent')
 const connector = require('ilp-connector')
 const Config = require('./config')
+const PeerFactory = require('../models/peer')
 
 module.exports = class Conncetor {
-  static constitute() { return [ Config ] }
-  constructor(config) {
+  static constitute() { return [ Config, PeerFactory ] }
+  constructor(config, Peer) {
     this.config = config.data
+    this.Peer = Peer
   }
 
   start() {
+    const self = this
     const port = process.env.CLIENT_PORT
     const ledgerPublicPath = this.config.getIn(['ledger', 'public_uri'])
 
     const interval = setInterval(() => {
       // Wait for the ledger and the proxy to start
-      request.get('0.0.0.0:' + port + '/' + ledgerPublicPath).end((err) => {
-        if (!err) {
-          clearInterval(interval)
+      request.get('0.0.0.0:' + port + '/' + ledgerPublicPath)
+        .end(co.wrap(function *(err) {
+          if (!err) {
+            clearInterval(interval)
 
-          connector.listen()
-        }
-      })
+            yield self.listen()
+          }
+        }))
     }, 2000)
+  }
+
+  * listen() {
+    // Start the connector
+    connector.listen()
+
+    // Add the peers
+    const peers = yield this.Peer.findAll()
+
+    peers.forEach(peer => {
+      //connector.addPlugin()
+    })
   }
 }
