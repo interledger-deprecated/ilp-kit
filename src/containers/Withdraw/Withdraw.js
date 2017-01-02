31c99721a59ada7c6f33f8c3e6329e3bd130fef8
Winthan <winthan@live.com>
2017-01-02 12:52:29 +0000

    Initial commit

---
+++ src/containers/Withdraw/Withdraw.js
@@ -0,0 +1,24 @@
+
+import React, {Component} from 'react'
+export default class Withdraw extends Component {
+  render() {
+  return (
+	<div className="panel panel-default">
+	       <div className="panel-heading">
+	         <div className="panel-title">Withdraw</div>
+	       </div>
+	       <div className="panel-body">
+
+		<h2>Cash Pickup Powered by Red Dot > </h2>
+		<p>Cash Pickup Powered by Red Dot allows you to withdraw cash from your Account for pickup at any Red Dot store located in the Myanmar.</p>
+
+
+
+		<h2>Transfer to a Bank > </h2>
+		<p>You can transfer funds from your  Account to a linked bank account for free.</p></div>
+
+	        </div>
+			)
+}
+}
+
