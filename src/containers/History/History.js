f13dc6882d6ca0da1f546f7a604bb6c4d6267e96
Vahe Hovhannisyan <vhpoet@gmail.com>
2017-01-04 13:47:12 -0800

    fix: History: pagination

--- src/containers/History/History.js
+++ src/containers/History/History.js
@@ -1,7 +1,7 @@
 import React, {Component, PropTypes} from 'react'
 import {connect} from 'react-redux'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
-import ReactPaginate from 'react-paginate';
+import ReactPaginate from 'react-paginate'
 import { getPage } from 'redux/actions/history'
 
 import { initActionCreators } from 'redux-pagination'
@@ -34,9 +34,33 @@ export default class Home extends Component {
     getPage: PropTypes.func
   }
 
+  state = {
+    list: []
+  }
+
   // Load the history
   componentDidMount() {
     this.props.getPage(1)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.loadingPage === nextProps.loadingPage) return
+
+    // This whole list thing is just for the ReactCSSTransitionGroup to work properly
+    // to not animate on initial load
+    this.setState({
+      list: []
+    })
+  }
+
+  componentDidUpdate() {
+    if (this.state.list === this.props.history) return
+
+    this.setState({
+      list: this.props.history
+    })
+
+    window.scrollTo(0, 0)
   }
 
   handlePageClick = data => {
@@ -46,45 +70,44 @@ export default class Home extends Component {
 
   render() {
     const {history, totalPages, loadingPage} = this.props
+    const {list} = this.state
 
     return (
       <div className={cx('container')}>
         <ul className={cx('list')}>
-          {history && history.length > 0 &&
-            <ReactCSSTransitionGroup transitionName={{
-              appear: cx('enter'),
-              appearActive: cx('enter-active'),
-              enter: cx('enter'),
-              enterActive: cx('enter-active'),
-              leave: cx('leave'),
-              leaveActive: cx('leave-active')
-            }} transitionAppearTimeout={1000} transitionEnterTimeout={1000} transitionLeaveTimeout={0}>
-            {history && history.map(item => {
-              return (
-                <li key={item.time_slot + item.source_identifier + item.destination_identifier + item.message}>
-                  <HistoryItem item={item} />
-                </li>
-              )
-            })}
-            </ReactCSSTransitionGroup>
-          }
+          {list.length > 0 && <ReactCSSTransitionGroup transitionName={{
+            appear: cx('enter'),
+            appearActive: cx('enter-active'),
+            enter: cx('enter'),
+            enterActive: cx('enter-active'),
+            leave: cx('leave'),
+            leaveActive: cx('leave-active')
+          }} transitionAppearTimeout={1000} transitionEnterTimeout={1000} transitionLeaveTimeout={50}>
+          {list.map(item => (
+            <li key={item.time_slot + item.source_identifier + item.destination_identifier + item.message}>
+              <HistoryItem item={item} />
+            </li>
+          ))}
+          </ReactCSSTransitionGroup>}
 
-          {loadingPage && <li className={cx('loading')}>Loading...</li>}
-          {!loadingPage && (!history || history.length === 0) && <li className={cx('loading')}>No payments to show</li>}
+          {!loadingPage && list.length === 0 && <li className={cx('loading')}>No payments to show</li>}
         </ul>
 
-        {history && history.length > 0 &&
-        <ReactPaginate
-          pageCount={totalPages || 1}
-          pageRangeDisplayed={5}
-          marginPagesDisplayed={1}
-          previousLabel="&laquo;"
-          nextLabel="&raquo;"
-          onPageChange={this.handlePageClick}
-          breakLabel={<span>...</span>}
-          containerClassName="pagination"
-          activeClassName="active"
-        />}
+        {history.length > 0 &&
+          <div className={cx('pagination')}>
+            <ReactPaginate
+              pageCount={totalPages || 1}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              previousLabel="&laquo;"
+              nextLabel="&raquo;"
+              onPageChange={this.handlePageClick}
+              breakLabel={<span>...</span>}
+              containerClassName="pagination"
+              activeClassName="active"
+            />
+            {loadingPage && <span className={cx('loading')}>Loading...</span>}
+          </div>}
       </div>
     )
   }
