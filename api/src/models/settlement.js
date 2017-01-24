7a4dc2fa9fd0d6bdfa2955cbaa54b6cb9a7673f6
Vahe Hovhannisyan <vhpoet@gmail.com>
2017-01-24 15:13:06 -0800

    feat: add settlements table

---
+++ api/src/models/settlement.js
@@ -0,0 +1,60 @@
+"use strict"
+
+module.exports = SettlementFactory
+
+const _ = require('lodash')
+const Container = require('constitute').Container
+const Model = require('five-bells-shared').Model
+const PersistentModelMixin = require('five-bells-shared').PersistentModelMixin
+const Database = require('../lib/db')
+const Validator = require('five-bells-shared/lib/validator')
+const Sequelize = require('sequelize')
+const PeerFactory = require('./peer')
+const SettlementMethodFactory = require('./settlement_method')
+
+SettlementFactory.constitute = [Database, Validator, Container]
+function SettlementFactory(sequelize, validator, container) {
+  class Settlement extends Model {
+    static convertFromExternal(data) {
+      return data
+    }
+
+    static convertToExternal(data) {
+      delete data.created_at
+      delete data.updated_at
+
+      return data
+    }
+
+    static convertFromPersistent(data) {
+      data = _.omit(data, _.isNull)
+      return data
+    }
+
+    static convertToPersistent(data) {
+      return data
+    }
+  }
+
+  Settlement.validateExternal = validator.create('Settlement')
+
+  PersistentModelMixin(Settlement, sequelize, {
+    id: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4
+    },
+    amount: Sequelize.FLOAT,
+    currency: Sequelize.STRING
+  })
+
+  container.schedulePostConstructor(Peer => {
+    Settlement.DbModel.belongsTo(Peer.DbModel)
+  }, [ PeerFactory ])
+
+  container.schedulePostConstructor(SettlementMethod => {
+    Settlement.DbModel.belongsTo(SettlementMethod.DbModel)
+  }, [ SettlementMethodFactory ])
+
+  return Settlement
+}
