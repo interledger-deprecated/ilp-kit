6fb266ef0643a4e9ae186dfbcd71a46bb8397171
Vahe Hovhannisyan <vhpoet@gmail.com>
2017-02-24 11:42:51 -0800

    fix: increase maxBuffer for dll generation

--- bin/dlls.js
+++ bin/dlls.js
@@ -4,6 +4,9 @@ require('./env').normalizeEnv()
 const exec = require('child_process').exec
 const cmd = 'node_modules/.bin/webpack --progress --config webpack/dll.config.js'
 
-exec(cmd, (error, stdout) => {
+exec(cmd, { maxBuffer: 1000 * 1024 }, (error, stdout) => {
+  if (error) {
+    console.error(error)
+  }
   console.log(stdout)
 })
