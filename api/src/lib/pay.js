4e96d448e2fadf72e9f905e84f7a63666dbd06ca
Vahe Hovhannisyan <vhpoet@gmail.com>
2017-03-31 16:30:25 -0700

    fix: SPSP format

--- api/src/lib/pay.js
+++ api/src/lib/pay.js
@@ -20,16 +20,11 @@ module.exports = class Pay {
 
   * pay (opts) {
     let transfer
-
-    /*const destination = yield this.utils.parseDestination({
-      destination: opts.destination
-    })*/
-
     /**
      * Ledger payment
      */
     try {
-      transfer = yield this.spsp.pay(opts.username, opts.payment)
+      transfer = yield this.spsp.pay(opts.user.username, opts.quote)
     } catch (e) {
       if (e.response && e.response.body && e.response.body.id && e.response.body.id === 'InsufficientFundsError') {
         throw new InsufficientFundsError()
@@ -42,19 +37,19 @@ module.exports = class Pay {
      * Store the payment in the wallet db
      */
     const payment = yield this.Payment.createOrUpdate({
-      source_user: opts.source.id,
-      source_identifier: this.utils.getWebfingerAddress(opts.source.username),
-      source_amount: parseFloat(opts.sourceAmount),
-      destination_identifier: destination.identifier,
-      destination_amount: parseFloat(opts.destinationAmount),
-      destination_name: destination.name,
-      destination_image_url: destination.imageUrl,
-      transfer: transfer.uuid,
-      message: opts.message || null,
-      execution_condition: transfer.executionCondition,
+      source_user: opts.user.id,
+      source_identifier: opts.user.identifier,
+      source_amount: parseFloat(opts.quote.sourceAmount),
+      destination_identifier: opts.destination.identifier,
+      destination_amount: parseFloat(opts.quote.destinationAmount),
+      // destination_name: destination.name,
+      // destination_image_url: destination.imageUrl,
+      transfer: opts.quote.id,
+      // message: opts.message || null,
+      // execution_condition: transfer.executionCondition,
       state: 'success'
     })
 
-    yield this.activity.processPayment(payment, opts.source)
+    yield this.activity.processPayment(payment, opts.user)
   }
 }
