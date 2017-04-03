211ed73025dc829ea2c2342af2a07d87298548a0
Michiel de Jong <michiel@ripple.com>
2017-04-03 16:59:31 +0200

    docs: Add pm2.config.js file and usage instructions

---
+++ pm2.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name      : 'ledger',
+      script    : './bin/ledger.js',
+      env: {
+        COMMON_VARIABLE: 'true',
+        NODE_PATH: './src',
+        NODE_ENV: 'production'
+      }
+    },
+    {
+      name      : 'api',
+      script    : './bin/api.js',
+      env: {
+        COMMON_VARIABLE: 'true',
+        NODE_PATH: './api',
+        NODE_ENV: 'production'
+      }
+    },
+    {
+      name      : 'web',
+      script    : './bin/server.js',
+      env: {
+        COMMON_VARIABLE: 'true',
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
