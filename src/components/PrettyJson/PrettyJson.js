9461140a0f126fb5aedd00804f526932c22e6a8f
Stefan Thomas <justmoon@members.fsf.org>
2017-05-10 00:23:23 -0700

    refactor: use prop-types module

--- src/components/PrettyJson/PrettyJson.js
+++ src/components/PrettyJson/PrettyJson.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import _extend from 'lodash/extend'
 
 import classNames from 'classnames/bind'
