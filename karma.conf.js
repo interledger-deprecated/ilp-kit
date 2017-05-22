b7e0d52d790fa5e01d07813b93bb4421bf5f879f
Vahe Hovhannisyan <vhpoet@gmail.com>
2017-05-22 15:44:59 -0700

    fix: scss loader for tests

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function (config) {
           { test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
           { test: /\.json$/, loader: 'json-loader' },
           { test: /\.less$/, loader: 'style!css!less' },
-          { test: /\.scss$/, loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap' }
+          { test: /\.scss$/, loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!postcss?sourceMap!resolve-url!sass?outputStyle=expanded&sourceMap' }
         ]
       },
       resolve: {
