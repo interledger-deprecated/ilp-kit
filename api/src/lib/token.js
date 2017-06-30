8843de7407e66f6585c73a312476263cebb68bd2
Dennis Appelt <dappelt@ripple.com>
2017-06-30 19:36:46 +0200

    refactor: creates/verifies json web token

--- api/src/lib/token.js
+++ api/src/lib/token.js
@@ -7,27 +7,34 @@ module.exports = class Token {
     this.secret = this.config.data.getIn(['api', 'secret'])
   }
 
-  get (prefix, duration) {
-    const expiresIn = isInfinity(duration)
-      ? null
-      : (Date.now() + duration) / 1000
-
-    return jsonWebToken.sign({
-      prefix,
-      expiresIn
-    }, this.secret)
+ /**
+ * Creates a Json Web Token for a given trustline prefix with an optional validity duration.
+ * 
+ * @param  {string} prefix   The trustline prefix for which the token is valid.
+ * @param  {number or string} expiresIn   Optional. Expressed in seconds or a string describing a time span. Eg: 60, "2 days", "10h", "7d"
+ * @return {string}   Json Web Token
+ */
+  get (prefix, expiresIn) {
+    return jsonWebToken.sign({}, this.secret, Object.assign({
+        algorithm: 'HS256',
+        subject: prefix
+      },
+      {
+        // TODO: make expiresIn a mandatory property of the json web token
+        expiresIn && {expiresIn}
+      })
+    )
   }
 
   isValid (prefix, token) {
     try {
-      const decoded = jsonWebToken.verify(token, this.secret, {
-        algorithms: [ 'HS256' ]
+      return jsonWebToken.verify(token, this.secret, {
+        algorithms: [ 'HS256' ],
+        subject: prefix
       })
     } catch (e) {
       debug('token validation error:', e.message)
       return false
     }
-
-    return (Date.now() < +decoded.expire) && (decoded.prefix === prefix)
   }
 }
