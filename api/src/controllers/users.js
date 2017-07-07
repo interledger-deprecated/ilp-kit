5d20296ce846154407004f49354cd3b26cf605fc
Dennis Appelt <dappelt@ripple.com>
2017-07-07 14:52:20 +0200

    fix: stores which user claimed an invite code

--- api/src/controllers/users.js
+++ api/src/controllers/users.js
@@ -177,9 +177,6 @@ function UsersControllerFactory (deps) {
 
           if (invite) {
             dbUser.invite_code = invite.code
-            invite.user_id = dbUser.id
-            // throws if the user identified by user_id has already claimed another invite
-            await invite.save(opts)
           } else if (!config.data.get('registration')) {
             throw new InvalidBodyError('The invite code is wrong')
           }
@@ -189,6 +186,12 @@ function UsersControllerFactory (deps) {
         try {
           dbUser = await dbUser.save(opts)
           dbUser = User.fromDatabaseModel(dbUser)
+
+          if (invite) {
+            invite.user_id = dbUser.id
+            // throws if the user identified by user_id has already claimed another invite
+            await invite.save(opts)
+          }
         } catch (e) {
           let errorMsg = e.errors && e.errors[0] && e.errors[0].message
           if (errorMsg === 'username must be unique') {
