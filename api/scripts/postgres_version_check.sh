9801ea78d7e57a7cc58792491c0cd81cfd85636c
Peter Krey <peterjkrey@gmail.com>
2017-08-21 12:20:46 -0700

    Add sort -V support for OSX

--- api/scripts/postgres_version_check.sh
+++ api/scripts/postgres_version_check.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine="Linux";;
+    Darwin*)    machine="Mac";;
+    CYGWIN*)    machine="Cygwin";;
+    MINGW*)     machine="MinGw";;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ "$machine" == "Mac" ]; then
+  brew install coreutils
+fi
+ 
 psql_version=$(psql --version)
 function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
 current_version=${psql_version//[^[:digit:].-]/}
