d96b145e57286cafd01d6620f6660cceea6d27ae
CircleCI <info@circleci.com>
2017-12-13 12:38:06 +0000

    chore: update gh-pages

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,1 +1,22 @@
-define({  "name": "ilp-kit-api",  "version": "4.0.0",  "description": "ILP kit API",  "title": "ILP kit",  "url": "https://wallet.example",  "order": [    "User",    "Payment",    "Misc",    "Auth"  ],  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-09-21T19:18:58.767Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
+define({
+  "name": "ilp-kit-api",
+  "version": "4.0.0",
+  "description": "ILP kit API",
+  "title": "ILP kit",
+  "url": "https://wallet.example",
+  "order": [
+    "User",
+    "Payment",
+    "Misc",
+    "Auth"
+  ],
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-12-13T12:38:05.881Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
