00f6976cc3a5e724e95ce40d98219ab255bce94e
Michiel de Jong <michiel@unhosted.org>
2018-11-23 13:17:55 +0100

    next: update current balances

--- index.js
+++ index.js
@@ -21,13 +21,11 @@ function snapOut(userId, obj) {
     if (results1[0].current + results1[0].receivable + obj.amount < results1[0].min) {
       throw new Error('peer could hit min balance');
     }
-    return runSql('INSERT INTO transactions (user_id, contact_id, requested_at, description,  direction, amount) ' +
-                  'VALUES                   ($1,      $2,         $3,           $4,           $5,         $6) RETURNING id', [
+    return runSql('INSERT INTO transactions (user_id, contact_id, requested_at, description,  direction, amount, status) ' +
+                  'VALUES                   ($1,      $2,         now (),       $3,           \'OUT\',    $4,     \'pending\') RETURNING id', [
       userId,
       results1[0].id,
-      new Date(),
       obj.description,
-      'OUT',
       obj.amount
     ]).then((results2) => {
       if (!results2 || !results2.length) {
@@ -196,7 +194,29 @@ hubbie.on('message', (peerName, message, userName) => {
   } catch(e) {
     throw  new Error('message not json');
   }
+
+  function updateStatus(newStatus) {
+    let userId;
+    let contactId; 
+    runSql('SELECT id FROM users WHERE name = $1', [ userName ]).then((results1) => {
+      if (!results1 || !results1.length) {
+        throw new Error('userName for incoming message not found');
+      }
+      userId = results1[0].id;
+      return runSql('SELECT id FROM contacts WHERE user_id = $1 AND name = $2', [ userId, peerName ]);
+    }).then((results2) => {
+      if (!results2 || !results2.length) {
+        throw new Error('peerName for incoming message not found');
+      }
+      contactId = results2[0].id;
+      return runSql('UPDATE transactions SET status = $1, responded_at = now() WHERE id = $2 AND user_id = $3 AND contact_id = $4 AND direction = \'OUT\' AND status = \'pending\'',
+          [ newStatus, obj.msgId, userId, contactId  ]);
+    });
+  }
+
   switch (obj.msgType) {
+    case 'ACCEPT': { updateStatus('accepted'); break; };
+    case 'REJECT': { updateStatus('rejected'); break; };
     case  'PROPOSE': {
       let userId;
       let status = 'accepted';
@@ -224,12 +244,10 @@ hubbie.on('message', (peerName, message, userName) => {
         channelName = userName + '/' + peerName; // match behavior of hubbie's internal channelName function
         hubbie.addClient({ peerUrl: results2[0].url, myName:/*fixme: hubbie should omit myName before mySecret in outgoing url*/ results2[0].token, peerName: channelName });
         return runSql('INSERT INTO transactions (user_id, contact_id, responded_at, description,  direction, amount, status) ' +
-                      'VALUES                   ($1,      $2,         $3,           $4,           $5,        $6,     $7) RETURNING id', [
+                      'VALUES                   ($1,      $2,         now (),       $3,           \'IN\',    $4,     $5) RETURNING id', [
           userId,
           results2[0].id,
-          new Date(),
           obj.description,
-          'IN',
           obj.amount,
           status
         ]);
@@ -244,12 +262,6 @@ hubbie.on('message', (peerName, message, userName) => {
           msgId: obj.msgId,
         }), userName);
       });
-      break;
-    };
-    case  'ACCEPT': {
-      break;
-    };
-    case  'REJECT': {
       break;
     };
   }
