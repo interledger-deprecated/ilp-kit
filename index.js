cb7029c9af81cce807f6342964334ca5292e965f
Michiel de Jong <michiel@unhosted.org>
2018-11-23 12:59:16 +0100

    accept proposals

--- index.js
+++ index.js
@@ -190,6 +190,69 @@ hubbie.on('peer', (eventObj) => {
 
 hubbie.on('message', (peerName, message, userName) => {
   console.log('hubbie message!', { peerName, message, userName });
+  let obj;
+  try {
+    obj = JSON.parse(message);
+  } catch(e) {
+    throw  new Error('message not json');
+  }
+  switch (obj.msgType) {
+    case  'PROPOSE': {
+      let userId;
+      let status = 'accepted';
+      let channelName; // see FIXME comment below
+      // resolve userName to userId
+      runSql('SELECT id FROM users WHERE name = $1', [ userName ]).then(results1 =>  {
+        if (!results1 || !results1.length) {
+          throw new Error('userName for incoming snap message not found');
+        }
+        userId = results1[0].id;
+        return runSql('SELECT id, url, token, min, payable, current, receivable, max FROM contacts WHERE user_id= $1  AND name = $2', [ userId, peerName ]);
+      }).then(results2 =>  {
+        if (!results2 || !results2.length) {
+          throw new Error('peerName for incoming message not found');
+        }
+        // their current balance will go down by amount
+        if (results2[0].current + results2[0].receivable - obj.amount < results2[0].min) {
+          status = 'rejected';
+        }
+        // in case of neg amount:
+        if (results2[0].current + results2[0].receivable - obj.amount > results2[0].max) {
+          status = 'rejected';
+        }
+        // FIXME: even when using http, maybe this peer should already exist if a message is being received from them?
+        channelName = userName + '/' + peerName; // match behavior of hubbie's internal channelName function
+        hubbie.addClient({ peerUrl: results2[0].url, myName:/*fixme: hubbie should omit myName before mySecret in outgoing url*/ results2[0].token, peerName: channelName });
+        return runSql('INSERT INTO transactions (user_id, contact_id, responded_at, description,  direction, amount, status) ' +
+                      'VALUES                   ($1,      $2,         $3,           $4,           $5,        $6,     $7) RETURNING id', [
+          userId,
+          results2[0].id,
+          new Date(),
+          obj.description,
+          'IN',
+          obj.amount,
+          status
+        ]);
+      }).then((results3) => {
+        if (!results3 || !results3.length) {
+          throw new Error('insert failed');
+        }
+        console.log('hubbie send', obj, results3,  userId, status);
+        // see FIXME comment above about peerBackName
+        return hubbie.send(peerName, JSON.stringify({
+          msgType: (status === 'accepted' ?  'ACCEPT' : 'REJECT'),
+          msgId: obj.msgId,
+        }), userName);
+      });
+      break;
+    };
+    case  'ACCEPT': {
+      break;
+    };
+    case  'REJECT': {
+      break;
+    };
+  }
 });
 
 hubbie.listen({
