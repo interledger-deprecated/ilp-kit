d3783a5f493800d2dcd32571c8da33dfa71d5b73
Michiel de Jong <michiel@unhosted.org>
2018-11-29 09:23:33 +0100

    test folder  lint

--- test/appSpec.js
+++ test/appSpec.js
@@ -1,5 +1,5 @@
-const App = require('../src/app')
-const assert = require('chai').assert
+const { assert } = require('chai');
+const App = require('../src/app');
 
 describe('App', function () {
   beforeEach(function () {
@@ -8,11 +8,11 @@ describe('App', function () {
   afterEach(function () {
   });
 
-  it('has a handler method', function ()  {
+  it('has a handler method', function () {
     assert.equal(typeof App.handler, 'function');
   });
 
-  it('has an initApp method', function ()  {
+  it('has an initApp method', function () {
     assert.equal(typeof App.initApp, 'function');
   });
 });
