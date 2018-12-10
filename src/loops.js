163ac6d64f0ce3d463d5f65e0439bffae0870792
Michiel de Jong <michiel@unhosted.org>
2018-12-10 13:38:47 +0100

    dedupe newTransaction function, fix #17

--- src/loops.js
+++ src/loops.js
@@ -1,50 +1,7 @@
 const hashlocks = require('hashlocks');
 const randomBytes = require('randombytes');
 const { runSql, getObject, getValue } = require('./db');
-const balances = require('./balances');
-
-async function newTransaction(userId, contact, transaction, direction) {
-  // console.log('newTransaction', userId, contact, transaction, direction);
-  const receivable = await balances.getMyReceivable(userId, contact.id);
-  const payable = await balances.getMyPayable(userId, contact.id);
-  const current = await balances.getMyCurrent(userId, contact.id);
-  if (direction === 'IN') {
-    // their current balance will go up by amount
-    // console.log(`CHECK1] ${current} + ${receivable} + ${transaction.amount} ?> ${contact.max}`);
-    if (current + receivable + transaction.amount > contact.max) {
-      // console.log(current, typeof current, receivable, typeof receivable,
-      //      transaction, typeof transaction.amount, contact, typeof contact.max);
-      throw new Error('peer could hit max balance (IN)');
-    }
-    // in case of neg amount:
-    // console.log(`CHECK2] ${current} - ${payable} + ${transaction.amount} ?< ${contact.min}`);
-    if (current - payable + transaction.amount < contact.min) {
-      throw new Error('peer could hit min balance (IN)');
-    }
-  }
-  if (direction === 'OUT') {
-    // their current balance will go down by amount
-    // console.log(`CHECK3] ${current} - ${payable} - ${transaction.amount} ?< ${contact.min}`);
-    if (current - payable - transaction.amount < contact.min) {
-      throw new Error('peer could hit min balance (OUT)');
-    }
-    // in case of neg amount:
-    // console.log(`CHECK4] ${current} + ${receivable} - ${transaction.amount} ?> ${contact.max}`);
-    if (current + receivable - transaction.amount > contact.max) {
-      throw new Error('peer could hit max balance (OUT)');
-    }
-  }
-  return getValue('INSERT INTO transactions '
-      + '(user_id, contact_id, msgid, requested_at, description,  direction, amount, status) VALUES '
-      + '($1,      $2,         $3,    now (),       $4,           $5,        $6,     \'pending\') RETURNING id AS value', [
-    userId,
-    contact.id,
-    transaction.msgId,
-    transaction.description,
-    direction,
-    transaction.amount,
-  ]);
-}
+const { newTransaction } = require('./ledger');
 
 async function loop(userId, obj, hubbie) {
   if (typeof userId !== 'number') {
--- test/contactsSpec.js
+++ test/contactsSpec.js
@@ -1,6 +1,5 @@
 const { assert } = require('chai');
 const fs = require('fs');
-const Hubbie = require('hubbie');
 const db = require('../src/db');
 const App = require('../src/app');
 
@@ -21,17 +20,16 @@ describe('Contacts', function () {
     await runSqlFile('./schema.sql');
     await runSqlFile('./fixture.sql');
     await db.runSql('DELETE FROM contacts');
-    this.hubbie = new Hubbie();
-    this.hubbie.send = function (peerName, msg, userName) {
-      assert.equal(peerName, 'name');
-      assert.equal(msg, JSON.stringify({
-        msgType: 'FRIEND-REQUEST',
-        url: 'undefined/michiel/name',
-        token: JSON.parse(msg).token,
-      }));
-      assert.equal(userName, 'michiel');
-      return Promise.resolve();
+    this.hubbie = {
+      addClient: () => {
+      },
+      send: (peerName, msg, userId) => {
+        this.snapSent.push({ peerName, msg, userId });
+      },
+      on: () => {
+      },
     };
+    this.snapSent = [];
     this.handler = App.makeHandler(this.hubbie);
     await new Promise(resolve => this.handler({
       headers: {
@@ -69,7 +67,13 @@ describe('Contacts', function () {
 
   it('creates a contact', async function () {
     const firstContact = await db.getObject('SELECT * FROM contacts LIMIT 1');
-    // console.log(firstContact);
+    assert.equal(this.snapSent[0].peerName, 'name');
+    assert.equal(this.snapSent[0].msg, JSON.stringify({
+      msgType: 'FRIEND-REQUEST',
+      url: 'undefined/michiel/name',
+      token: JSON.parse(this.snapSent[0].msg).token,
+    }));
+    assert.equal(this.snapSent[0].userId, 'michiel');
     assert.deepEqual(firstContact, {
       user_id: 1,
       id: 8,
