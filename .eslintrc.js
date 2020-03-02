305eb4dc7e6a38ccb40c516295ff906a5eb81761
Michiel de Jong <michiel@unhosted.org>
2020-03-02 14:01:05 +0100

    rename .js to .ts

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
+    es6: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint"
+  ]
+};
